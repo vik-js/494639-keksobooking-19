e4883173d53d7a50cca65a195f556b0aed90aa8c
vik-js <emillia@bk.ru>
2020-02-20 00:12:58 +0500

    правки замечаний

--- js/main.js
+++ js/main.js
@@ -11,15 +11,15 @@ var MAPS = document.querySelector('.map'); // находим класс map
 MAPS.classList.remove('map--faded'); // удаляем у map класс map--faded
 
 // функция выбора рендомного числа
-var intRand = function getRandomInt(min, max) {
+function getRandomInt(min, max) {
   min = Math.ceil(min); // округление в большую сторону
   max = Math.floor(max); // округление в меньшую сторону
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 // функция выбора нескольких рендомных значений
-var createRamdArrayMany = function (array) {
-  var numberArray = intRand(0, array.length); // выбор значений из массива от 0 до числа длины массива
+var createRandArrayMany = function (array) {
+  var numberArray = getRandomInt(0, array.length); // выбор значений из массива от 0 до числа длины массива
   var newArray = []; // новый массив
   for (var i = 0; i < numberArray; i++) { // выбираем столько элементов, сколько отоболось рендомно
     newArray.push(array[i]); // складываем элементы в массив newArray
@@ -28,15 +28,14 @@ var createRamdArrayMany = function (array) {
 };
 
 // функция выбора одного рендомного значения
-var createRamdArrayOne = function (array) {
-  var newElement = intRand(0, array.length); // выбираем 1 рендомное число от 0 до длины массива
+var createRandArrayOne = function (array) {
+  var newElement = getRandomInt(0, array.length); // выбираем 1 рендомное число от 0 до длины массива
   return newElement; // возвращаем выбранное рендомно число
 };
 
-var data = []; // создаем пустой массив
-
 // создание 8-ми элементов
 var createDataMoke = function () {
+  var data = []; // создаем пустой массив
   for (var i = 0; i < 8; i++) {
     data.push({
       author: {
@@ -46,43 +45,46 @@ var createDataMoke = function () {
         title: 'Заголовок ' + i,
         address: '600, 350',
         price: 0,
-        type: createRamdArrayOne(TYPE),
+        type: createRandArrayOne(TYPE),
         rooms: 4,
         guests: 10,
-        checkin: createRamdArrayOne(CHECKIN),
-        checkout: createRamdArrayOne(CHECKOUT),
-        features: createRamdArrayMany(FEATURES),
+        checkin: createRandArrayOne(CHECKIN),
+        checkout: createRandArrayOne(CHECKOUT),
+        features: createRandArrayMany(FEATURES),
         description: 'Строка с описанием' + i,
-        photos: createRamdArrayMany(PHOTOS)
+        photos: createRandArrayMany(PHOTOS)
       },
       location: {
-        x: intRand(0, 1200),
-        y: intRand(130, 630)
+        x: getRandomInt(0, 1200),
+        y: getRandomInt(130, 630)
       }
     });
   }
   return data;
+
 };
 
-createDataMoke();
+var data = createDataMoke();
 
 var pinElement = document.querySelector('.map__pins'); // находим класс в котором будут выводится точки на карте
 var pinTamplate = document.querySelector('#pin').content; // находим id шаблона в верстке и получаем его содержимое
+
+// заполняем аватарку
+var renderAvatar = function () {
+  var iconWidth = 50;
+  var iconHeight = 70;
+  var pinBtnTamplate = pinTamplate.querySelector('button').cloneNode(true); // клонируем весь шаблон button
+  pinTamplate.querySelector('.map__pin').style = 'left: ' + (data[i].location.x + iconWidth / 2) + 'px; top: ' + (data[i].location.y - iconHeight) + 'px;'; // в шаблоне button находим атрибут style и задаем новое значение
+  pinBtnTamplate.querySelector('img').src = data[i].author.avatar; // находим изображение аватарки и задаем значение атрибуту src
+  pinBtnTamplate.querySelector('img').alt = data[i].offer.title; // находим изображения аватарки и задаем значение атрибуду alt
+  return pinBtnTamplate;
+};
+
 var fragment = document.createDocumentFragment(); // создаем пустой фрагмент
 
 for (var i = 0; i < data.length; i++) {
-  // заполняем аватарку
-  var renderAvatar = function () {
-    var pinBtnTamplate = pinTamplate.querySelector('button').cloneNode(true); // клонируем весь шаблон button
-    pinTamplate.querySelector('.map__pin').style = 'left: ' + data[i].location.x + 'px; top: ' + data[i].location.y + 'px;'; // в шаблоне button находим атрибут style и задаем новое значение
-    pinBtnTamplate.querySelector('img').src = data[i].author.avatar; // находим изображение аватарки и задаем значение атрибуту src
-    pinBtnTamplate.querySelector('img').alt = data[i].offer.title; // находим изображения аватарки и задаем значение атрибуду alt
-
-    return pinBtnTamplate;
-  };
-
-  renderAvatar();
-  fragment.appendChild(renderAvatar(data)); // добавляем в фрагмент аватарку
+  fragment.appendChild(renderAvatar(data[i])); // добавляем в фрагмент аватарки
   pinElement.appendChild(fragment); // выводим фрагмент в верстку
 }
+
 
